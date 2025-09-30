86f001512f6b42a5fff953730e26c4ffe5ac88b9
ardho206 <syanirsstn@gmail.com>
2025-09-30 09:12:30 +0700

    add footer content

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,74 @@
+import { Instagram, Mail, MapPin, Phone } from "lucide-react";
+import Image from "next/image";
+
 export default function Footer() {
   return (
-    <footer className="p-4 text-center bg-gray-200 fixed bottom-0 left-0 w-full">
-      <p>© 2025 IGTPPB - All Rights Reserved</p>
-      <p>INI FOOTER</p>
+    <footer className="bg-[#F7F1E7]">
+      <div className="flex flex-col md:flex-row md:items-start items-start justify-between gap-10 px-6 py-10 max-w-6xl mx-auto">
+        {/* logo */}
+        <div className="flex justify-center md:justify-start w-full md:w-auto order-1">
+          <Image
+            src="/Images/igttpb_logo.jpg"
+            alt="Logo"
+            width={120}
+            height={120}
+            className="rounded-full object-cover"
+          />
+        </div>
+
+        {/* contact */}
+        <div className="text-[#686232] space-y-2.5 w-full md:w-auto order-4 md:order-2 ">
+          <h2 className="text-xl font-bold text-center md:text-start">Contact Us</h2>
+          <p className="mt-2 text-sm flex items-center">
+            <MapPin className="mr-2 shrink-0" size={21} />
+            Jl. Terusan Ryacudu · (0721) 8030188
+          </p>
+          <p className="mt-1.5 text-sm flex items-center">
+            <Mail className="mr-2 shrink-0" size={21} />
+            emailexample@example.com
+          </p>
+          <p className="mt-1.5 text-sm flex items-center">
+            <Phone className="mr-2 shrink-0" size={21} />
+            +1 (123) 456-7890
+          </p>
+          <p className="mt-1.5 text-sm flex items-center">
+            <Instagram className="mr-2 shrink-0" size={21} />
+            @informatics.goestpb
+          </p>
+        </div>
+
+        {/* pages */}
+        <div className="text-[#686232] space-y-2.5 w-full md:w-auto order-1 md:order-3 text-center md:text-start">
+          <h2 className="text-xl font-bold">Pages</h2>
+          <ul className="space-y-1.5 mt-2 font-medium">
+            <li><a href="#" className="text-sm">Home</a></li>
+            <li><a href="#" className="text-sm">About</a></li>
+            <li><a href="#" className="text-sm">Blog</a></li>
+            <li><a href="#" className="text-sm">Contact</a></li>
+          </ul>
+        </div>
+
+        {/* about */}
+        <div className="text-[#686232] space-y-2.5 w-full md:max-w-xs order-2 md:order-4 text-center md:text-start">
+          <h2 className="text-xl font-bold">About</h2>
+          <p className="mt-2 text-sm">
+            IGTTPB merupakan First Gathering Mahasiswa Teknik Informatika di Institut Teknologi Sumatera.
+            IGTTPB berperan sebagai wadah perkenalan diri untuk saling mengenal satu sama lain.
+          </p>
+        </div>
+      </div>
+
+
+      {/* bottom bar */}
+      <div className="w-full bg-[#686232] text-[#F7F1E7] py-2 text-center overflow-hidden">
+        <div className="max-w-6xl mx-auto px-2">
+          <p className="whitespace-nowrap text-[10px] sm:text-[12px] md:text-sm">
+            Copyright © 2025. IGTTPB25 - All Rights Reserved | Developed by Algovista
+          </p>
+        </div>
+      </div>
+
+
     </footer>
   );
 }
