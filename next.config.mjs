0a3bdaeaa5ea577e828ae99b54964e3afcd5b02e
Yoel <febrianyoel.contact@gmail.com>
2025-10-02 18:15:56 +0700

    Update image remote patterns in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-                port: '',
-                pathname: '/ddzjskfyn/**',
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'igttpb.site', // Ganti dengan domain gambar Anda
+        // Anda juga bisa menambahkan port dan pathname jika perlu
+      },
+    ],
+  },
 };
 
 export default nextConfig;
