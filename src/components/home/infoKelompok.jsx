ca3c867371b7f4cc76b54118bc2039f5ab4684e3
Afnnimadurrosyad <afnanimadurrosyad@gmail.com>
2025-10-02 11:27:49 +0700

    fix: update import path for HasilPencarian component

--- src/components/home/infoKelompok.jsx
+++ src/components/home/infoKelompok.jsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState } from 'react';
 import { findPesertaByNim } from '../../api/pesertaApi2.jsx'; // Impor fungsi API (sesuaikan path)
-import HasilPencarian from './HasilPencarian'; // Impor komponen hasil (sesuaikan path)
+import HasilPencarian from '@components/home/HasilPencarian'; // Impor komponen hasil (sesuaikan path)
 
 function InfoKelompok() {
   const [nimInput, setNimInput] = useState('');
