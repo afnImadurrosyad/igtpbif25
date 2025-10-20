b95fa9c1908e17fdecc03acb69e65b25599c7501
RifatSyauki <rifatlampung658@gmail.com>
2025-10-20 13:50:24 +0700

    test login

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,5 +1,16 @@
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { createClient } from '@supabase/supabase-js';
+
+let supabaseClient = null;
+
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
+
+export const getSupabaseClient = () => {
+  if (!supabaseClient) {
+    supabaseClient = createBrowserSupabaseClient();
+  }
+  return supabaseClient;
+};
