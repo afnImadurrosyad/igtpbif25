ccc0e0359190331890efa11b0353395df599b3f7
Afnnimadurrosyad <afnanimadurrosyad@gmail.com>
2025-10-21 22:58:56 +0700

    fixin some bugs

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -13,7 +13,7 @@ export default function LoginPage() {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}`,
+          redirectTo: `${window.location.origin}/`,
         },
       });
     } catch (error) {
