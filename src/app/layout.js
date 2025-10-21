4c4385b9053afe2c807758a87e5b945defa5cb75
Afnnimadurrosyad <afnanimadurrosyad@gmail.com>
2025-10-21 22:49:04 +0700

    Fiksasi login dan logout dari navbar utama

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,33 +1,32 @@
-import localFont from "next/font/local";
-import "./globals.css";
+import localFont from 'next/font/local';
+import './globals.css';
+import { AuthProvider } from '../contexts/AuthContext';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata = {
-  title: "IGTTPB 2025",
-  description: "Informatics Goes To TPB 2025 - Institut Teknologi Sumatera",
+  title: 'IGTTPB 2025',
+  description: 'Informatics Goes To TPB 2025 - Institut Teknologi Sumatera',
   icons: {
-    icon: "https://res.cloudinary.com/ddzjskfyn/image/upload/v1759423035/igttpb2025logo_lfcrjn.webp",
-  }
+    icon: 'https://res.cloudinary.com/ddzjskfyn/image/upload/v1759423035/igttpb2025logo_lfcrjn.webp',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-
+    <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
