9ac40e31f07367778af4694934a606e0e23abfd5
Afnnimadurrosyad <afnanimadurrosyad@gmail.com>
2025-10-21 19:44:52 +0700

    fixing BUGGGS not bug

--- src/pages/login.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// import { getSupabaseClient } from '../utils/supabaseClient';
-import GoogleLoginButton from '../components/login/googleLogin';
-
-export default function Login() {
-  return (
-    <GoogleLoginButton />
-  );
-}
