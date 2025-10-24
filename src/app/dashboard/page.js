49989060f63752676194e56a464382c9b4a093dd
Afnnimadurrosyad <afnanimadurrosyad@gmail.com>
2025-10-24 21:53:46 +0700

    bisa pls

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,24 +1,26 @@
 'use client';
-import { useEffect } from 'react';
-import { useAuth } from '../../contexts/AuthContext';
+import { useEffect, useState } from 'react';
+import { useAuth } from '@/contexts/AuthContext';
 import DashAdmin from '@/components/dashboard/dashAdmin';
 import DashPeserta from '@/components/dashboard/dashPeserta';
 
 export default function Page() {
+  const [roles, setRoles] = useState(null);
   const { role, checkRole } = useAuth();
 
   useEffect(() => {
-    if (!role) {
+    if (!roles || !role) {
       const interval = setInterval(async () => {
         console.log('⏳ Mengecek role ulang...... role saat ini' + role);
         await checkRole();
+        setRoles(role);
       }, 1000);
 
       return () => clearInterval(interval); // bersihkan interval saat unmount
     }
-  }, [role, checkRole]);
+  }, [role, checkRole, roles]);
 
-  if (!role) {
+  if (!roles) {
     console.log('🚫 Belum ada role');
     return (
       <div className='flex justify-center items-center min-h-screen'>
@@ -27,12 +29,12 @@ export default function Page() {
     );
   }
 
-  if (role === 'user') {
+  if (roles === 'user') {
     console.log('✅ Role user terdeteksi');
     return <DashPeserta />;
   }
 
-  if (['admin', 'mentor', 'daplok'].includes(role)) {
+  if (['admin', 'mentor', 'daplok'].includes(roles)) {
     console.log('✅ Role admin/mentor/daplok terdeteksi');
     return <DashAdmin />;
   }
