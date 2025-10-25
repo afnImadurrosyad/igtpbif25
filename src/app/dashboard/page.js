54aa6bebd625d311d11bb1cff899d8f284c959e2
Afnnimadurrosyad <afnanimadurrosyad@gmail.com>
2025-10-26 02:42:32 +0700

    fixing role on dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,15 +1,48 @@
 'use client';
+import { useEffect, useState } from 'react';
 import DashAdmin from '@/components/dashboard/dashAdmin';
 import DashPeserta from '@/components/dashboard/dashPeserta';
 import { getRoleFromLocal } from '@/utils/localRole';
-import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Page() {
-  const [role, setRole] = useState();
-  setRole(getRoleFromLocal());
-  if (role == 'user') {
+  const router = useRouter();
+  const [role, setRole] = useState(null);
+
+  const handleHome = () => {
+    router.push('/');
+  };
+  // ambil role dari localStorage hanya sekali saat komponen mount
+  useEffect(() => {
+    const storedRole = getRoleFromLocal();
+    setRole(storedRole);
+  }, []);
+
+  // tampilkan loading sementara role belum terambil
+  if (!role) {
+    console.log('role pasti null =', role);
+    return (
+      <div className='min-h-screen flex items-center justify-center text-gray-700'>
+        Memuat dashboard...
+      </div>
+    );
+  }
+
+  // arahkan sesuai role
+  if (role === 'user') {
+    console.log('aku user bang');
     return <DashPeserta />;
-  } else if (role == 'admin') {
+  } else if (role === 'admin' || role === 'mentor' || role === 'daplok') {
+    console.log('anjay aku admin');
     return <DashAdmin />;
+  } else {
+    return (
+      <div>
+        <div className='min-h-screen flex items-center justify-center text-gray-700'>
+          <div>Role tidak dikenali. atau program error</div>
+          <button onClick={handleHome}>Kembali ke Beranda</button>
+        </div>
+      </div>
+    );
   }
 }
